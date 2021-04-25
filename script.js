cd6a63d3e6f4bdce67618ef136a4281ddf5541e7
ViltonPrint <83180372+ViltonPrint@users.noreply.github.com>
2021-04-25 22:57:32 +0400

    Adding pages

---
+++ script.js
@@ -0,0 +1,267 @@
+//menu page start
+function myFunction() {
+  // Get the checkbox
+  var checkBox1 = document.getElementById("myCheck1");
+
+  var checkBox2 = document.getElementById("myCheck2");
+  var checkBox3 = document.getElementById("myCheck3");
+  // Get the output text
+  var menu1 = document.getElementById("menu1");
+  var menu2 = document.getElementById("menu2");
+  var menu3 = document.getElementById("menu3");
+  var menu4 = document.getElementById("menu4");
+  var menu5 = document.getElementById("menu5");
+  var menu6 = document.getElementById("menu6");
+  var menu7 = document.getElementById("menu7");
+  var menu8 = document.getElementById("menu8");
+  var menu9 = document.getElementById("menu9");
+
+  // If the checkbox is checked, display the output text
+    if (checkBox1.checked == true){
+  } else {
+    menu1.style.display = "block";
+    menu2.style.display = "block";
+    menu3.style.display = "block";
+    menu4.style.display = "block";
+    menu5.style.display = "block";
+    menu6.style.display = "block";
+    menu7.style.display = "block";
+    menu8.style.display = "block";
+    menu9.style.display = "block";
+  }
+
+
+  if (checkBox2.checked == true){
+    menu2.style.display = "none";
+    menu3.style.display = "none";
+    menu4.style.display = "none";
+    menu6.style.display = "none";
+    menu8.style.display = "none";
+    menu9.style.display = "none";
+
+  } else {
+    menu1.style.display = "block";
+    menu2.style.display = "block";
+    menu3.style.display = "block";
+    menu4.style.display = "block";
+    menu5.style.display = "block";
+    menu6.style.display = "block";
+    menu7.style.display = "block";
+    menu8.style.display = "block";
+    menu9.style.display = "block";
+  }
+
+    if (checkBox3.checked == true){
+    menu1.style.display = "none";
+    menu2.style.display = "none";
+    menu3.style.display = "none";
+    menu5.style.display = "none";
+    menu7.style.display = "none";
+    menu8.style.display = "none";
+    menu9.style.display = "none";
+  } 
+  else {
+
+  }
+
+
+}
+
+//menu page end
+//vizitka page start
+
+function myCardFunction() {
+  // Get the checkbox
+  var VcheckBox1 = document.getElementById("checkbox-v-1");
+  var VcheckBox2 = document.getElementById("checkbox-v-2");
+  var VcheckBox3 = document.getElementById("checkbox-v-3");
+  var VcheckBox4 = document.getElementById("checkbox-v-4");
+  // Get the output text
+  var BusinessCard1 = document.getElementById("BusinessCard1");
+  var BusinessCard2 = document.getElementById("BusinessCard2");
+  var BusinessCard3 = document.getElementById("BusinessCard3");
+  var BusinessCard4 = document.getElementById("BusinessCard4");
+  var BusinessCard5 = document.getElementById("BusinessCard5");
+  var BusinessCard6 = document.getElementById("BusinessCard6");
+  var BusinessCard7 = document.getElementById("BusinessCard7");
+  var BusinessCard8 = document.getElementById("BusinessCard8");
+  var BusinessCard9 = document.getElementById("BusinessCard9");
+  var BusinessCard10 = document.getElementById("BusinessCard10");
+  var BusinessCard11 = document.getElementById("BusinessCard11");
+  var BusinessCard12 = document.getElementById("BusinessCard12");
+  var BusinessCard13 = document.getElementById("BusinessCard13");
+  var BusinessCard14 = document.getElementById("BusinessCard14");
+  var BusinessCard15 = document.getElementById("BusinessCard15");
+  var BusinessCard16 = document.getElementById("BusinessCard16");
+  var BusinessCard17 = document.getElementById("BusinessCard17");
+  var BusinessCard18 = document.getElementById("BusinessCard18");
+  var BusinessCard19 = document.getElementById("BusinessCard19");
+  var BusinessCard20 = document.getElementById("BusinessCard20");
+  var BusinessCard21 = document.getElementById("BusinessCard21");
+  var BusinessCard22 = document.getElementById("BusinessCard22");
+  var BusinessCard23 = document.getElementById("BusinessCard23");
+  var BusinessCard24 = document.getElementById("BusinessCard24");
+  var BusinessCard25 = document.getElementById("BusinessCard25");
+  var BusinessCard26 = document.getElementById("BusinessCard26");
+  var BusinessCard27 = document.getElementById("BusinessCard27");
+  var BusinessCard28 = document.getElementById("BusinessCard28");
+  var BusinessCard29 = document.getElementById("BusinessCard29");
+
+  // If the checkbox is checked, display the output text
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+    if (VcheckBox1.checked == true){
+  } else {
+    BusinessCard1.style.display = "block";
+    BusinessCard2.style.display = "block";
+    BusinessCard3.style.display = "block";
+    BusinessCard4.style.display = "block";
+    BusinessCard5.style.display = "block";
+    BusinessCard6.style.display = "block";
+    BusinessCard7.style.display = "block";
+    BusinessCard8.style.display = "block";
+    BusinessCard9.style.display = "block";
+    BusinessCard10.style.display = "block";
+    BusinessCard11.style.display = "block";
+    BusinessCard12.style.display = "block";
+    BusinessCard13.style.display = "block";
+    BusinessCard14.style.display = "block";
+    BusinessCard15.style.display = "block";
+    BusinessCard16.style.display = "block";
+    BusinessCard17.style.display = "block";
+    BusinessCard18.style.display = "block";
+    BusinessCard19.style.display = "block";
+    BusinessCard20.style.display = "block";
+    BusinessCard21.style.display = "block";
+    BusinessCard22.style.display = "block";
+    BusinessCard23.style.display = "block";
+    BusinessCard24.style.display = "block";
+    BusinessCard25.style.display = "block";
+    BusinessCard26.style.display = "block";
+    BusinessCard27.style.display = "block";
+    BusinessCard27.style.display = "block";
+    BusinessCard28.style.display = "block";
+  }
+
+
+  if (VcheckBox2.checked == true){
+    BusinessCard13.style.display = "none";
+    BusinessCard14.style.display = "none";
+    BusinessCard19.style.display = "none";
+    BusinessCard20.style.display = "none";
+    BusinessCard21.style.display = "none";
+    BusinessCard22.style.display = "none";
+    BusinessCard23.style.display = "none";
+
+
+  } else {
+    BusinessCard1.style.display = "block";
+    BusinessCard2.style.display = "block";
+    BusinessCard3.style.display = "block";
+    BusinessCard4.style.display = "block";
+    BusinessCard5.style.display = "block";
+    BusinessCard6.style.display = "block";
+    BusinessCard7.style.display = "block";
+    BusinessCard8.style.display = "block";
+    BusinessCard9.style.display = "block";
+    BusinessCard10.style.display = "block";
+    BusinessCard11.style.display = "block";
+    BusinessCard12.style.display = "block";
+    BusinessCard13.style.display = "block";
+    BusinessCard14.style.display = "block";
+    BusinessCard15.style.display = "block";
+    BusinessCard16.style.display = "block";
+    BusinessCard17.style.display = "block";
+    BusinessCard18.style.display = "block";
+    BusinessCard19.style.display = "block";
+    BusinessCard20.style.display = "block";
+    BusinessCard21.style.display = "block";
+    BusinessCard22.style.display = "block";
+    BusinessCard23.style.display = "block";
+    BusinessCard24.style.display = "block";
+    BusinessCard25.style.display = "block";
+    BusinessCard26.style.display = "block";
+    BusinessCard27.style.display = "block";
+    BusinessCard28.style.display = "block";
+  }
+
+    if (VcheckBox3.checked == true){
+    BusinessCard1.style.display = "none";
+    BusinessCard2.style.display = "none";
+    BusinessCard3.style.display = "none";
+    BusinessCard4.style.display = "none";
+    BusinessCard5.style.display = "none";
+    BusinessCard6.style.display = "none";
+    BusinessCard7.style.display = "none";
+    BusinessCard8.style.display = "none";
+    BusinessCard9.style.display = "none";
+    BusinessCard10.style.display = "none";
+    BusinessCard11.style.display = "none";
+    BusinessCard12.style.display = "none";
+    BusinessCard13.style.display = "none";
+    BusinessCard14.style.display = "none";
+    BusinessCard15.style.display = "none";
+    BusinessCard16.style.display = "none";
+    BusinessCard17.style.display = "none";
+    BusinessCard18.style.display = "none";
+    BusinessCard21.style.display = "none";
+    BusinessCard22.style.display = "none";
+    BusinessCard24.style.display = "none";
+    BusinessCard25.style.display = "none";
+    BusinessCard26.style.display = "none";
+    BusinessCard27.style.display = "none";
+    BusinessCard28.style.display = "none";
+  } 
+  else {
+
+  }
+
+      if (VcheckBox4.checked == true){
+    BusinessCard1.style.display = "none";
+    BusinessCard2.style.display = "none";
+    BusinessCard3.style.display = "none";
+    BusinessCard4.style.display = "none";
+    BusinessCard5.style.display = "none";
+    BusinessCard6.style.display = "none";
+    BusinessCard7.style.display = "none";
+    BusinessCard8.style.display = "none";
+    BusinessCard9.style.display = "none";
+    BusinessCard10.style.display = "none";
+    BusinessCard11.style.display = "none";
+    BusinessCard12.style.display = "none";
+    BusinessCard15.style.display = "none";
+    BusinessCard16.style.display = "none";
+    BusinessCard17.style.display = "none";
+    BusinessCard18.style.display = "none";
+    BusinessCard19.style.display = "none";
+    BusinessCard20.style.display = "none";
+    BusinessCard23.style.display = "none";
+    BusinessCard24.style.display = "none";
+    BusinessCard25.style.display = "none";
+    BusinessCard26.style.display = "none";
+    BusinessCard27.style.display = "none";
+    BusinessCard27.style.display = "none";
+    BusinessCard28.style.display = "none";
+  } 
+  else {
+
+  }
+
+
+}
+
+
+
+
+//vizitka page end
